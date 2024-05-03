a647430242e2f907a83467f5605d2c91f2d81e8f
Dhanush <dhanushkrish54@gmail.com>
2024-05-03 09:29:15 +0530

    03-05-2024 | 09:27AM
    Added post mapping to add mob details with image to db and display it in webpage. Successfully implemented with validations. Edit products is pending.

--- src/main/java/com/dhanush/moshop/controllers/ProductsController.java
+++ src/main/java/com/dhanush/moshop/controllers/ProductsController.java
@@ -1,17 +1,31 @@
 package com.dhanush.moshop.controllers;
 
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.dhanush.moshop.models.Product;
 import com.dhanush.moshop.models.ProductDto;
 import com.dhanush.moshop.services.ProductsRepository;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/products")
@@ -32,5 +46,71 @@ public class ProductsController {
 		ProductDto productDto = new ProductDto();
 		model.addAttribute("productDto", productDto);
 		return "products/CreateProduct";
+	}
+	
+	@PostMapping("/create")
+	public String createProduct(
+		@Valid @ModelAttribute ProductDto productDto,
+		BindingResult result
+		) {
+		
+		if(productDto.getImageFile().isEmpty()) {
+			result.addError(new FieldError("productDto", "imageFile", "The image file is required"));
+		}
+		if(result.hasErrors()) {
+			return "products/CreateProduct";
+		}
+		
+		//save image file
+		MultipartFile image = productDto.getImageFile();
+		Date createdAt = new Date();
+		String storageFileName = createdAt.getTime() + "_" + image.getOriginalFilename();
+		
+		try {
+			String uploadDir = "Public/images/";
+			Path uploadPath = Paths.get(uploadDir);
+			
+			if(!Files.exists(uploadPath)) {
+				Files.createDirectories(uploadPath);
+			}
+			
+			try (InputStream inputStream = image.getInputStream()) {
+				Files.copy(inputStream, Paths.get(uploadDir + storageFileName),
+						StandardCopyOption.REPLACE_EXISTING);
+			}
+		} catch(Exception ex) {
+			System.out.println("Exception: " + ex.getMessage());
+		}
+		
+		Product product = new Product();
+		product.setName(productDto.getName());
+		product.setBrand(productDto.getBrand());
+		product.setCategory(productDto.getCategory());
+		product.setPrice(productDto.getPrice());
+		product.setDescription(productDto.getDescription());
+		product.setCreatedAt(createdAt);
+		product.setImageFileName(storageFileName);
+		
+		repo.save(product);
+		
+		return "redirect:/products";
+	}
+	
+	@GetMapping("/edit")
+	public String showEditPage(
+		Model model,
+		@RequestParam int id
+		) {
+		try {
+			Product product = repo.findById(id).get();
+			model.addAttribute("product", product);
+			
+			
+			
+		} catch(Exception ex) {
+			System.out.println("Exception: " + ex.getMessage());
+			return "redirect:/products";
+		}
+		return "products/EditProduct";
 	}
 }
